609d06e87ed0248449de740cf9a4d21278059d49
qitas <qitas@qitas.cn>
2019-11-08 20:25:01 +0800

    add thread init

---
+++ examples/thread/scripts/copy.py
@@ -0,0 +1,24 @@
+Import("env")
+import shutil
+import os
+#
+# Dump build environment (for debug)
+# print env.Dump()
+#
+
+#
+# Upload actions
+#
+
+
+exe_source = os.path.join(env.subst("$BUILD_DIR"), "program.exe")
+if os.path.exists(exe_source):
+	shutil.copy(exe_source, '.')
+
+def after_build(source, target, env):
+	shutil.copy(exe_source, 'exe')
+
+env.AddPostAction("buildprog", after_build)
+
+# exe_source = os.path.join(env.subst("$BUILD_DIR"), "program.exe")
+# print(exe_source)
