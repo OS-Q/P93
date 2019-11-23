df524ab6d99c3f543a162792b5e5908ae46cd9ca
qitas <qitas@qitas.cn>
2019-11-23 12:50:47 +0800

    fix

--- examples/thread/src/main.c
+++ examples/thread/src/main.c
@@ -1,8 +1,35 @@
+
+#define WIN32_LEAN_AND_MEAN
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+
+DWORD WINAPI ThreadFunc(LPVOID);
+
 int main()
 {
-    printf("Hello PlatformIO for Win32!\n");
-    system("pause");
-    return 0;
+　HANDLE hThrd;
+　DWORD threadId;
+　int i;
+
+　for (i = 0; i < 5; i++)
+　{
+　　hThrd = CreateThread(NULL, 0, ThreadFunc, (LPVOID)i, 0, &threadId);
+　　if (hThrd)
+　　{
+　　　printf("Thread launched %d\n", i);
+　　　CloseHandle(hThrd);
+　　}
+　}
+　// Wait for the threads to complete.
+　Sleep(2000);
+　return EXIT_SUCCESS;
+}
+
+DWORD WINAPI ThreadFunc(LPVOID n)
+{
+　int i;
+　for (i = 0; i < 10; i++)
+　　printf("%d%d%d%d%d%d%d%d\n", n, n, n, n, n, n, n, n);
+　return 0;
 }
